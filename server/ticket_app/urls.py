68031b27a2dc1637698d395a3844afcc1258d4cf
Oswaldo <oramirez10102@gmail.com>
2025-12-30 04:38:00 -0500

    Created url patterns for tickets

--- server/ticket_app/urls.py
+++ server/ticket_app/urls.py
@@ -1,6 +1,14 @@
-from .views import TicketView
+from .views import TicketView, TicketsPurchasedView, TicketTemplatesView
 from django.urls import path
 
 urlpatterns = [
+    # url for Stripe payment 
     path('purchase/', TicketView.as_view(), name='ticket_purchase'),
+    
+    # view all user's purchased tickets
+    path('purchased_tickets/',TicketsPurchasedView.as_view(), name="purchased_tickets"),
+    
+    # view all ticket templates 
+    path('ticket_templates/', TicketTemplatesView.as_view(), name="ticket_templates")
+
 ]
