261dc0c985217c824b88ed1b8bb1f4ca418ccb5b
Oswaldo <oramirez10102@gmail.com>
2025-12-31 07:40:27 -0500

    I am the Lorax. I speak for the trees

--- server/comment_app/serializers.py
+++ server/comment_app/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
 from .models import Comment
 
+# Iterative serializer
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ["author", "parent", "event", "text", "time", "likes"]
-        read_only_fields = ["author", "parent"]
+        fields = ["id", "author", "parent", "event", "text", "time", "likes"]
+        read_only_fields = ["author"]
+
+class CommentRecursiveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ["id", "author", "parent", "event", "text", "time", "likes"]
+        read_only_fields = ["author"]
+    # Recursively grab the replies of the comment
+    def get_replies(self, curr_comment):
+        replies = curr_comment.replies.all().order_by("time")
+        return CommentSerializer(replies, many=True).data
