e7a89faaee9306662acfa191a9a9baf02259f13b
Samuel Bordenkecher <stb4380@gmail.com>
2025-12-31 16:29:39 -0500

    Some serializer work done

--- server/payments_app/serializers.py
+++ server/payments_app/serializers.py
@@ -2,8 +2,25 @@ from rest_framework import serializers
 from .models import Payment
 
 class PaymentSerializer(serializers.ModelSerializer):
-    user = serializers.PrimaryKeyRelatedField(read_only=True)
-    # changed to break circular imports
+    user_id = serializers.IntegerField(source="order.user.id", read_only=True)
+    user_email = serializers.EmailField(source="order.user.email", read_only=True)
+    order_id = serializers.IntegerField(source="order.id", read_only=True)
+
+    class Meta:
+        model = Payment
+        fields = ['id', 'order_id', 'user_id', 'user_email', 'amount', 'stripe_payment_intent_id', 'status', 'created_at']
+        read_only_fields = ['id', 'order_id', 'user_id', 'user_email', 'stripe_payment_intent_id', 'created_at']
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    pass
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    pass
+
+
+
+
     """
     # Add user details via method field if needed
     user_email = serializers.SerializerMethodField()
@@ -15,8 +32,3 @@ class PaymentSerializer(serializers.ModelSerializer):
     def get_user_name(self, obj):
         return obj.ticket.user.full_name
     """
-
-    class Meta:
-        model = Payment
-        fields = '__all__'
-        read_only_fields = ['created_at', 'lesson', 'stripe_payment_intent_id']
