b2aac42a2beb05a4a53b74911d3f1069fd07cc08
Oswaldo <oramirez10102@gmail.com>
2026-01-01 15:48:02 -0500

    Cleaned up ticket_app to remove unncessary code that will ultimately live in payment_app

--- server/ticket_app/tests/test_tickets.py
+++ server/ticket_app/tests/test_tickets.py
@@ -1,9 +1,6 @@
-import threading
 import pytest
-from django.db.models import Sum
 
 from ticket_app.serializers import TicketSerializer
-from ticket_app.models import Ticket
 
 
 @pytest.mark.django_db
@@ -29,91 +26,3 @@ def test_ticket_serializer_rejects_zero_quantity(premium_template):
         data={"ticket": premium_template.id, "quantity": 0}
     )
     assert not serializer.is_valid()
-
-
-@pytest.mark.django_db
-def test_cannot_oversell_single_template(
-    auth_client,
-    user,
-    premium_template,
-):
-    Ticket.objects.create(
-        user=user,
-        ticket=premium_template,
-        quantity=3,
-    )
-
-    response = auth_client.post(
-        "/ticket/purchased_tickets/",
-        data={
-            "ticket": premium_template.id,
-            "quantity": 2,
-        },
-        format="json",
-    )
-
-    assert response.status_code == 400
-    assert "Only 1 tickets" in str(response.data)
-
-
-@pytest.mark.django_db
-def test_global_premium_limit_enforced(
-    auth_client,
-    user,
-    premium_template,
-):
-    Ticket.objects.create(
-        user=user,
-        ticket=premium_template,
-        quantity=4,
-    )
-
-    response = auth_client.post(
-        "/ticket/purchased_tickets/",
-        data={
-            "ticket": premium_template.id,
-            "quantity": 1,
-        },
-        format="json",
-    )
-
-    assert response.status_code == 400
-
-
-@pytest.mark.django_db(transaction=True)
-def test_concurrent_purchases_do_not_oversell(
-    auth_client,
-    user,
-    premium_template,
-):
-    results = []
-
-    def attempt_purchase():
-        response = auth_client.post(
-            "/ticket/purchased_tickets/",
-            data={
-                "ticket": premium_template.id,
-                "quantity": 3,
-            },
-            format="json",
-        )
-        results.append(response.status_code)
-
-    t1 = threading.Thread(target=attempt_purchase)
-    t2 = threading.Thread(target=attempt_purchase)
-
-    t1.start()
-    t2.start()
-
-    t1.join()
-    t2.join()
-
-    assert sorted(results) == [201, 400]
-
-    total_sold = (
-        Ticket.objects
-        .filter(ticket=premium_template)
-        .aggregate(total=Sum("quantity"))["total"]
-    )
-
-    assert total_sold == 3
--- server/ticket_app/urls.py
+++ server/ticket_app/urls.py
@@ -1,17 +1,8 @@
-from .views import TicketView,TicketTemplatesView,TicketPurchasedView, TicketPurchasedListView
+from .views import TicketTemplatesView
 from django.urls import path
 
 urlpatterns = [
-    # url for Stripe payment 
-    path('purchase/', TicketView.as_view(), name='ticket_purchase'),
-    
-    # view all user's purchased tickets
-    path('purchased_tickets/',TicketPurchasedView.as_view(), name="purchased_tickets"),
-    
     # view all ticket templates 
     path('ticket_templates/', TicketTemplatesView.as_view(), name="ticket_templates"),
-
-    # view my purchased tickets
-    path('my-tickets/', TicketPurchasedListView.as_view(), name="my-tickets")
 
 ]
