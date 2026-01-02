a8671cfad5c5d0ce89ef92ca3194974b64207b7d
tom <thomlankford@gmail.com>
2026-01-02 07:23:27 -0800

    Gradual implementation of Tailwind+Motion, Navbar replaced

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// import './App.css'
 import './index.css'
 import router from './router.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
